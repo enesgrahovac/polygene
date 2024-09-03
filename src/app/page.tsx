06af0548c3109c3e3d667d6109e2eeae61f600d4
Enes Grahovac <enesxgrahovac@gmail.com>
2024-09-03 14:55:54 -0400

    attempt rebuild

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import LandingPage from "@/components/pageContent/Landing/Landing";
+
+export default function Home() {
+  return (
+    <main>
+        <LandingPage />
+    </main>
+  );
+}
+
